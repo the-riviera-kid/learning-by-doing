821cb3c1af086cae03e72b5ce5e214ac756513fe
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-02-06 11:17:23 +0100

    Add guessing_game initial setup

---
+++ 06-02-23-functions/guessing_game/guessing_game.py
@@ -0,0 +1,11 @@
+'''
+    The classic number guessing game.
+    The computer picks a number between 1 and 100, and asks you to guess.
+    It tells you if you were too high or too low, and gives you six chances to guess.
+'''
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
