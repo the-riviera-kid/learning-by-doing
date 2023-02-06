a8ea5e056331a8759a7e7be24a9805bb9d2d75f6
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-02-06 11:51:40 +0100

    Modify Problem Steps to include if the user wants to play again

--- 06-02-23-functions/guessing_game/guessing_game.py
+++ 06-02-23-functions/guessing_game/guessing_game.py
@@ -10,12 +10,16 @@
     3. Ask user to guess a number between 1 and 100
     4. Decrease the guesses count by 1
     5. Check if the guessed number is correct
-    6. If correct, user wins - print 'You guessed correctly!' and end the game
-    7. If the number is too high - print 'Your guess is too high'
-    8. Repeat steps 2 - 6
-    9. If the number is too low - print 'Your guess is too low'
-    10. Repeat steps 2 - 6
-    11. When guess == 0, print 'Too bad, you've run out of guesses' and end the game
+    6. If correct, user wins - print 'You guessed correctly!'
+    7. Check if user wants to play again - Print 'Would you like to play again? y/n '
+    8. If 'y' return to step 1 and set number of guesses back to 6
+    9. If 'n' exit()
+    10. If the number is too high - print 'Your guess is too high'
+    11. Return to step 2
+    12. If the number is too low - print 'Your guess is too low'
+    13. Return to step 2
+    14. When guesses == 0, print 'Too bad, you've run out of guesses'
+    15. Return to step 7
 '''
 
 def main():
