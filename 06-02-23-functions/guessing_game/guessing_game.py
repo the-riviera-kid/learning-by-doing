a96a25acf55e8b1871abf11c99fa68faddceabf7
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-02-06 11:43:12 +0100

    Add 'Problem Steps'

--- 06-02-23-functions/guessing_game/guessing_game.py
+++ 06-02-23-functions/guessing_game/guessing_game.py
@@ -1,11 +1,27 @@
 '''
+    Challenge:
     The classic number guessing game.
     The computer picks a number between 1 and 100, and asks you to guess.
     It tells you if you were too high or too low, and gives you six chances to guess.
+
+    Problem Steps:
+    1. Give the user the rules to play the game (including that they have 6 chances to guess)
+    2. Print - You have {number} of guesses
+    3. Ask user to guess a number between 1 and 100
+    4. Decrease the guesses count by 1
+    5. Check if the guessed number is correct
+    6. If correct, user wins - print 'You guessed correctly!' and end the game
+    7. If the number is too high - print 'Your guess is too high'
+    8. Repeat steps 2 - 6
+    9. If the number is too low - print 'Your guess is too low'
+    10. Repeat steps 2 - 6
+    11. When guess == 0, print 'Too bad, you've run out of guesses' and end the game
 '''
 
 def main():
     pass
+
+
 
 if __name__ == '__main__':
     main()
