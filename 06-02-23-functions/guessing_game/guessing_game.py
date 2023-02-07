3e40f0b52a6a16bece9b3471a1aef43251c03df9
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-02-07 17:11:15 +0100

    Add line spaces between functions to adhere to PEP8 guidlines

--- 06-02-23-functions/guessing_game/guessing_game.py
+++ 06-02-23-functions/guessing_game/guessing_game.py
@@ -6,21 +6,21 @@
 
     Problem Steps:
     1. Computer gets a random number between 1 and 100
-    1. Give the user the rules to play the game (including that they have 6 chances to guess)
-    2. Print - You have {number} guesses
-    3. Ask user to guess a number between 1 and 100
-    4. Decrease the guesses count by 1
-    5. Check if the guessed number is correct
-    6. If correct, user wins - print 'You guessed correctly!'
-    7. Check if user wants to play again - Print 'Would you like to play again? y/n '
-    8. If 'y' return to step 1 and set number of guesses back to 6
-    9. If 'n' exit()
-    10. If the number is too high - print 'Your guess is too high'
-    11. Return to step 2
-    12. If the number is too low - print 'Your guess is too low'
-    13. Return to step 2
-    14. When guesses == 0, print 'Too bad, you've run out of guesses'
-    15. Return to step 7
+    2. Give the user the rules to play the game (including that they have 6 chances to guess)
+    3. Print - You have {number} guesses
+    4. Ask user to guess a number between 1 and 100
+    5. Decrease the guesses count by 1
+    6. Check if the guessed number is correct
+    7. If correct, user wins - print 'You guessed correctly!'
+    8. Check if user wants to play again - Print 'Would you like to play again? y/n '
+    9. If 'y' return to step 1 and set number of guesses back to 6
+    10. If 'n' exit()
+    11. If the number is too high - print 'Your guess is too high'
+    12. Return to step 2
+    13. If the number is too low - print 'Your guess is too low'
+    14. Return to step 2
+    15. When guesses == 0, print 'Too bad, you've run out of guesses'
+    16. Return to step 7
 '''
 
 from random import randint
@@ -51,9 +51,11 @@ def game_rules():
     print()
     print(f'You have 6 guesses.')
 
+
 def get_random_number():
     number = randint(1, 101)
     return number
+
 
 def get_user_number():
     print()
@@ -61,15 +63,18 @@ def get_user_number():
     print()
     return guess
 
+
 def number_too_high(user_guess, target_number):
     if user_guess > target_number:
         print('Your guess is too high')
         return True
 
+
 def number_too_low(user_guess, generated_number):
     if user_guess < generated_number:
         print('Your guess is too low')
         return True
+
 
 def incorrect_guess_countdown(too_high, too_low, guesses_remaining):
     if too_high or too_low:
@@ -79,6 +84,7 @@ def incorrect_guess_countdown(too_high, too_low, guesses_remaining):
             print()
         return True
 
+
 def check_user_wins(user_guess, generated_number):
     if user_guess == generated_number:
         print('Congratulations, you guessed the correct number!')
@@ -86,6 +92,7 @@ def check_user_wins(user_guess, generated_number):
         return replay()
     else:
         return False
+
 
 def replay():
     play_again = input('Would you like to play again? (y/n) ')
@@ -95,6 +102,7 @@ def replay():
     else:
         print()
         exit()
+
 
 if __name__ == '__main__':
     main()
