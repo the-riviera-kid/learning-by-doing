eed378bdcb29955e2735b0b7c8db332564e86eda
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-02-07 15:48:23 +0100

    Neaten up presentation

--- 06-02-23-functions/guessing_game/guessing_game.py
+++ 06-02-23-functions/guessing_game/guessing_game.py
@@ -46,17 +46,19 @@ def play_game(random_number, guesses):
 
 
 def game_rules():
+    print()
     print("This is a number guessing game. \nYou have 6 chances to guess the number correctly. \nLet's play!")
     print()
     print(f'You have 6 guesses.')
 
 def get_random_number():
     number = randint(1, 101)
-    print(number) # For the sake of creation
     return number
 
 def get_user_number():
+    print()
     guess = int(input('Guess a number between 1 and 100: '))
+    print()
     return guess
 
 def number_too_high(user_guess, target_number):
@@ -72,11 +74,15 @@ def number_too_low(user_guess, generated_number):
 def incorrect_guess_countdown(too_high, too_low, guesses_remaining):
     if too_high or too_low:
         print(f'You have {guesses_remaining} guesses remaining')
+        if guesses_remaining == 0:
+            print(f'Better luck next time!')
+            print()
         return True
 
 def check_user_wins(user_guess, generated_number):
     if user_guess == generated_number:
-        print('You guessed correctly!')
+        print('Congratulations, you guessed the correct number!')
+        print()
         return replay()
     else:
         return False
@@ -84,8 +90,10 @@ def check_user_wins(user_guess, generated_number):
 def replay():
     play_again = input('Would you like to play again? (y/n) ')
     if play_again == 'y':
+        print()
         main()
     else:
+        print()
         exit()
 
 if __name__ == '__main__':
