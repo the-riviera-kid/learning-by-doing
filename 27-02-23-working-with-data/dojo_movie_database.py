78b311c5f772abaafab4cfda091c1e7361a91dc9
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-03-13 10:43:02 +0100

    Add dojo_movie_database challenge with first 3 optionsand 'Quit' complete

--- 27-02-23-working-with-data/dojo_movie_database.py
+++ 27-02-23-working-with-data/dojo_movie_database.py
@@ -1,4 +1,4 @@
-'''
+'''quit
     dojo_movie_database.py : This program will import the movie data, and then provide a menu of options to the user. 
     The user can select an option, the program will do it, and afterwards the program should return to the main menu. 
     
@@ -20,42 +20,113 @@ def main():
     main_menu = True
     while main_menu:
         menu_option = menu_options()
-        option_choice = navigate(menu_option, movie_list, main_menu)
-        print(option_choice)
+        navigate(menu_option, movie_list, main_menu)
+        input('\nPress enter to return to the main menu ')
+
 
 
 def menu_options():
-    option = input('\nWhat would you like to do?\
+    option = int(input('\nWhat would you like to do?\
            \n\
            \nOption 1: Display all Movies\
            \nOption 2: Search Movies by Actor\
            \nOption 3: Search Movies by Genre\
            \nOption 4: Add Movie\
-           \nOption 5: Quit\n\n')
+           \nOption 5: Quit\n\n'))
     return option
 
 
+# Borrowed from Gijs ter Haar
 def navigate(menu_option, movie_list, main_menu):
-    if menu_option == '1':
-        display_all_movies(movie_list)
-        input('Press any key to return to the main menu ')
-        return main_menu
-    elif menu_option == '2':
-        return 2
-    elif menu_option == '3':
-        return 3
-    elif menu_option == '4':
-        return 4
-    else:
-        exit()
+    menu_list = [display_all_movies, search_movies_by_actor, search_movies_by_genre, add_movie_to_db, quit]
+    return menu_list[menu_option -1](movie_list, main_menu)
 
 
-def display_all_movies(movie_list):
+def display_all_movies(movie_list, main_menu):
     for movie in movie_list:
         title = movie['title']
         year = movie['year']
         print(f'{title}, {year}')
+    return main_menu
+
+
+def search_movies_by_actor(movie_list, main_menu):
+    requested_actor = input("Which Actor's Movies would you like to see? ")
+    movies_starring_actor = {}
+    for movie in movie_list:
+        for actor in movie['actors']:
+            if requested_actor.title() in actor:
+                if actor in movies_starring_actor:
+                    movies_starring_actor[actor].append({movie['title']: movie['year']})
+                else:
+                    movies_starring_actor[actor] = [{movie['title']: movie['year']}]
+    return print_movies_starring_actor(movies_starring_actor, main_menu, requested_actor, movie_list)
+
+
+def print_movies_starring_actor(movies_starring_actor, main_menu, requested_actor, movie_list):
+    if movies_starring_actor:
+        for actor, movie in movies_starring_actor.items():
+            print(f'\n{actor} starred in:')
+            for movie_dict in movie:
+                for title, year in movie_dict.items():
+                    print(f'{title} in the year {year}') 
+        return main_menu
+    print(f'Sorry, we have no movies starring "{requested_actor}" in our database.')
+    return search_movies_by_actor(movie_list, main_menu)
+        
+
+
+def search_movies_by_genre(movie_list, main_menu):
+    genre = input('Which genre would you like to see? \n\n')
+    print()
+    for movie in movie_list:
+        if genre.lower() in movie['genre']:
+            print(movie['title'], movie['year'])
+    if genre.lower() not in movie['genre']:
+        print(f'Sorry, we have no "{genre}" movies in our database.')
+        search_movies_by_genre(movie_list, main_menu)
+    return main_menu
+
+
+def add_movie_to_db(movie_list):
+    print('Add to list')
+
+
+def quit(movie_list, main_menu):
+    exit()
 
 
 if __name__ == '__main__':
     main()
+
+
+'''
+
+def navigate(menu_option, movie_list, main_menu):
+    if menu_option == '1':
+        display_all_movies(movie_list)
+        input('\nPress enter to return to the main menu ')
+        return main_menu
+    elif menu_option == '2':
+        search_movies_by_actor(movie_list)
+        input('\nPress enter to return to the main menu ')
+        return main_menu
+    elif menu_option == '3':
+        search_movies_by_genre(movie_list)
+        input('\nPress enter to return to the main menu ')
+        return main_menu
+    elif menu_option == '4':
+        add_movie_to_db(movie_list)
+        return 4
+    else:
+        exit()
+
+The nicer way is to not print anything inside the loops at all. 
+Make a new list outside the loop called movies_starring_actor or something, 
+and add the movies (or just the titles and years) to that list as you go through the loops. 
+Then, at the end, after the loops, You can print the contents. 
+The nice thing about this is that you're keeping your processing and your output separate. 
+It also means that you know if any movies were found, 
+so you can print something like "Sorry, but 'Sander Hilgena' is not in any movies."
+
+'''
