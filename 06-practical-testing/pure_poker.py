e3d4deaf08fc3815ae3711105cac04ddc8ca4147
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-04-20 14:21:25 +0200

    Further refactoring

--- 06-practical-testing/pure_poker.py
+++ 06-practical-testing/pure_poker.py
@@ -24,14 +24,23 @@ def check_hand_is_invalid(user_input):
                 return True
         return False
             
-            
 def get_poker_description(user_input): # '10H JH QC KD AS'
     card_list = get_card_data(user_input)
     rank_list, suit_list, card = get_data(card_list)
     count_dict = get_count_dict(rank_list)
-    poker_hand = check_ranks(count_dict, rank_list, suit_list, card)
-    return poker_hand
-    
+    rank_list_numbers, rank_list_other, suites = get_rank_lists(suit_list, rank_list, card)
+
+    poker_hand = check_same_kinds(count_dict)
+    if poker_hand is not None:
+        return poker_hand
+    poker_hand = check_unique_card_hands(rank_list_numbers, suites)
+    if poker_hand is not None:
+        return poker_hand
+    poker_hand = check_straight(rank_list_numbers, rank_list_other)
+    if poker_hand is not None:
+        return poker_hand
+    return 'High Card'
+
 
 def get_card_data(user_input):
     card_list = []
@@ -39,6 +48,7 @@ def get_card_data(user_input):
         card = parse_card(c)
         card_list.append(card)
     return card_list
+
 
 def get_data(card_list):
     rank_list = []
@@ -48,6 +58,7 @@ def get_data(card_list):
         suit_list.append(card['suit'])
     return rank_list, suit_list, card
 
+
 def get_count_dict(rank_list):
     count_dict = {}
     for rank in rank_list:
@@ -55,7 +66,14 @@ def get_count_dict(rank_list):
     return count_dict # {'10': 1, 'jack': 1, 'queen': 1, 'king': 1, 'ace': 1}
 
 
-def check_ranks(count_dict, rank_list, suit_list, card):
+def get_rank_lists(suit_list, rank_list, card):
+    rank_list_numbers = [int(rank) for rank in rank_list if rank.isnumeric()]
+    rank_list_other = [rank for rank in rank_list if not rank.isnumeric()]
+    suites = suit_list.count(card['suit'])
+    return rank_list_numbers, rank_list_other, suites
+
+
+def check_same_kinds(count_dict):
     if 4 in count_dict.values():
         return 'Four Of A Kind'
     elif 3 in count_dict.values() and 2 in count_dict.values():
@@ -65,23 +83,27 @@ def check_ranks(count_dict, rank_list, suit_list, card):
     elif 2 in count_dict.values():
         poker_hand = check_count(count_dict)
         return poker_hand
-    elif 1 in count_dict.values():
-        poker_hand = check_unique_card_hands(rank_list, suit_list, card)
-        if poker_hand is not None:
-            return poker_hand
-    return 'High Card'
+
+def check_count(count_dict):
+    count = 0
+    for v in count_dict.values():
+        if v == 2:
+            count += 1
+    if count == 1:
+        return 'One Pair'
+    elif count == 2:
+        return 'Two Pair'
 
 
-def check_unique_card_hands(rank_list, suit_list, card):
-    rank_list_numbers = [int(rank) for rank in rank_list if rank.isnumeric()]
-    rank_list_other = [rank for rank in rank_list if not rank.isnumeric()]
-    suites = suit_list.count(card['suit'])
-    
+def check_unique_card_hands(rank_list_numbers, suites):
     if sorted(rank_list_numbers) == sorted(list(range(min(rank_list_numbers), max(rank_list_numbers)+1))) and suites == 5:
         return 'Straight Flush'
     elif suites == 5:
         return 'Flush'
-    elif sorted(rank_list_numbers) == sorted(list(range(min(rank_list_numbers), max(rank_list_numbers)+1))):
+
+
+def check_straight(rank_list_numbers, rank_list_other):
+    if sorted(rank_list_numbers) == sorted(list(range(min(rank_list_numbers), max(rank_list_numbers)+1))):
         if len(rank_list_numbers) == 5:
             return 'Straight'
         elif len(rank_list_numbers) == 4 and 'jack' in rank_list_other or 'ace' in rank_list_other:
@@ -93,13 +115,3 @@ def check_unique_card_hands(rank_list, suit_list, card):
         elif len(rank_list_numbers) == 1 and 'jack' in rank_list_other and 'queen' in rank_list_other and 'king' in rank_list_other and 'ace' in rank_list_other:
             return 'Straight'
         
-
-def check_count(count_dict):
-    count = 0
-    for v in count_dict.values():
-        if v == 2:
-            count += 1
-    if count == 1:
-        return 'One Pair'
-    elif count == 2:
-        return 'Two Pair'
