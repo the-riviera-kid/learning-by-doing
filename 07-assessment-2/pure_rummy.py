4e921cf1c4607ad13101d7bbbdb6b682cf05fb55
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-05-22 10:14:26 +0200

    Test for invalid user input

--- 07-assessment-2/pure_rummy.py
+++ 07-assessment-2/pure_rummy.py
@@ -1,7 +1,8 @@
-def rummy():
-    is_valid = check_valid()
+def rummy(user_input):
+    is_valid = check_valid(user_input)
     return is_valid
 
 
-def check_valid():
-    pass
+def check_valid(user_input):
+    if user_input is None or user_input == '' or not isinstance(user_input, str):
+        return 'Sorry, that is invalid'
--- 07-assessment-2/test_pure_rummy.py
+++ 07-assessment-2/test_pure_rummy.py
@@ -1,6 +1,14 @@
 from pure_rummy import rummy
 
 
-def test_rummy_exists():
-    assert rummy() == None
-    
+def test_input_invalid_none():
+    result =  rummy(None)
+    assert result == 'Sorry, that is invalid'
+
+def test_input_invalid_empty_string():
+    result =  rummy('')
+    assert result == 'Sorry, that is invalid'
+
+def test_input_invalid_integer():
+    result =  rummy(5)
+    assert result == 'Sorry, that is invalid'
