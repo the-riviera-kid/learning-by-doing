f001932dac2fded1f0229cb28b4c50f49c920bd5
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-07-05 08:34:40 +0200

    Add mathematical functionality to Card

--- 08-OOP-i-did-it-again/card.py
+++ 08-OOP-i-did-it-again/card.py
@@ -5,6 +5,7 @@ RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 class Card:
     def __init__(self, card):
         self._card = self._is_card_valid(card)
+        # self._rank = self._check_rank_type(self._card)
         self._rank = self._card[:-1]
         self._suit = self._card[-1]
 
@@ -15,3 +16,17 @@ class Card:
                 card[-1] not in SUITS:
             raise TypeError('Sorry, that is invalid')
         return card
+
+    # def _check_rank_type(self, card):
+    #     if card[:-1].isnumeric():
+    #         return int(card[:-1])
+    #     return card[:-1]
+    
+    def __gt__(self, other): # 'KH', '4C'
+        return RANKS.index(self._rank) > RANKS.index(other._rank)
+        
+    def __lt__(self, other): # 'KH', '4C'
+        return RANKS.index(self._rank) < RANKS.index(other._rank)
+        
+    def __sub__(self, other): # 'QH', '10C'
+        return RANKS.index(self._rank) - RANKS.index(other._rank) # return 10 - 8 -> 10 & 8 are the indexes of 'Q' & '10'
--- 08-OOP-i-did-it-again/test_card.py
+++ 08-OOP-i-did-it-again/test_card.py
@@ -40,6 +40,52 @@ def test_card_has_rank():
     rank = Card('5H')._rank
     assert rank == '5'
 
+# def test_card_has_numbered_rank():
+#     rank = Card('5H')._rank
+#     assert rank == 5
+
+# def test_card_has_letter_rank():
+#     rank = Card('AH')._rank
+#     assert rank == 'A'
+
 def test_card_has_suit():
     suit = Card('5H')._suit
     assert suit == 'H'
+
+def test_carlas_idea():
+    card1 = Card('5H')
+    card2 = Card('4C')
+    assert card2._rank < card1._rank
+
+def test_dans_idea_greater_than():
+    card1 = Card('5H')
+    card2 = Card('4C')
+    assert card2 < card1
+
+def test_greater_than_letter():
+    card1 = Card('KH')
+    card2 = Card('6C')
+    assert card2 < card1
+
+def test_less_than_number():
+    card1 = Card('4C')
+    card2 = Card('5H')
+    assert card2 > card1
+
+def test_minus_numbers():
+    card1 = Card('4C')
+    card2 = Card('5H')
+    result = card2 - card1
+    assert result == 1
+
+def test_minus_letters():
+    card1 = Card('10C')
+    card2 = Card('QH')
+    result = card2 - card1
+    assert result == 2
+
+def test_minus_False():
+    card1 = Card('6C')
+    card2 = Card('8H')
+    result = card2 - card1
+    assert result != 1
