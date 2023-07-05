eb237e2632c4384aaf2a71d5ac2af0039b401c97
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-07-05 08:35:40 +0200

    Add pokerhand checks to PokerHand

--- 08-OOP-i-did-it-again/poker_hand.py
+++ 08-OOP-i-did-it-again/poker_hand.py
@@ -7,19 +7,55 @@ class PokerHand:
         self._hand = [Card(card) for card in self._hand] # check if each card is valid and make a new list
         # Card(card) is creating an object of the class Card
         # a list of 5 Card objects
+        self._ranks = [card._rank for card in self._hand] # ['3', '8', '8', '9', '3'] -> ['3', '8', '9']
+        self._rank_counts = {r:self._ranks.count(r) for r in self._ranks}
 
     def _is_hand_valid(self, hand):
         if not isinstance(hand, str) or hand == '' or \
-            len(hand.split()) != 5:
+            len(set(hand.split())) != 5:
             raise TypeError('Sorry, your poker hand is invalid')
         return hand.split()
 
     def get_hand_name(self):
-        rank = [card._rank for card in self._hand] # ['A', '10', '5', '10', '6']
-        if len(set(rank)) == 4:
-            return 'One Pair'
-        return 'High Card'
+        CHECKS = ((self._check_full_house, 'Full House'),
+                  (self._check_four_of_a_kind, 'Four Of A Kind'),
+                  (self._check_one_pair, 'One Pair'),
+                  (self._check_three_of_a_kind, 'Three Of A Kind'),
+                  (self._check_two_pair, 'Two Pair'),
+                  (self._check_straight, 'Straight'),
+                  (self._check_high_card, 'High Card'),)
+        
+        for check, hand in CHECKS:
+            if check():
+                return hand
+    
+    def _check_full_house(self): # ['4', '4', '4,' '8', '8'] -> ['4', '8']
+        for r in self._rank_counts:
+            return len(set(self._ranks)) == 2 and self._rank_counts[r] == 3
+    
+    def _check_four_of_a_kind(self):
+        return len(set(self._ranks)) == 2
+    
+    def _check_one_pair(self):
+        return len(set(self._ranks)) == 4
 
+    def _check_three_of_a_kind(self): # ['6', '6', '7', 'J', '6'] -> ['6', '7', 'J']
+        for r in self._rank_counts:
+            return len(set(self._ranks)) == 3 and self._rank_counts[r] == 3
+    
+    def _check_two_pair(self):
+        return len(set(self._ranks)) == 3
+    
+    def _check_straight(self):
+        sorted_cards = sorted(self._hand)
+        for i in range(len(sorted_cards)-1):
+            if sorted_cards[i+1] - sorted_cards[i] != 1:
+                return False
+        return True
+
+    def _check_high_card(self):
+        return 'High Card'
+    
 
 '''
 CHECKS = ((full_house, 'Full House', 7),
--- 08-OOP-i-did-it-again/test_poker_hand.py
+++ 08-OOP-i-did-it-again/test_poker_hand.py
@@ -28,10 +28,46 @@ def test_invalid_cards():
     with pytest.raises(TypeError):
         PokerHand('2H 4D 6S 10C 6Z')
 
+def test_invalid_reject_duplicate_cards():
+    with pytest.raises(TypeError):
+        PokerHand('JC JD JS JC 5H')
+
 def test_high_card():
     hand = PokerHand('AS 10S 5H 7C 6S').get_hand_name()
     assert hand == 'High Card'
 
-def test_high_card():
+def test_one_pair():
     hand = PokerHand('AS 10S 5H 10C 6S').get_hand_name()
     assert hand == 'One Pair'
+
+def test_two_pair():
+    hand = PokerHand('3H 8C 8H 9S 3D').get_hand_name()
+    assert hand == 'Two Pair'
+
+def test_three_of_a_kind():
+    hand = PokerHand('6S 6H 7C JD 6D').get_hand_name()
+    assert hand == 'Three Of A Kind'
+
+def test_four_of_a_kind():
+    hand = PokerHand('JC JD JS JH 5H').get_hand_name()
+    assert hand == 'Four Of A Kind'
+
+def test_full_house():
+    hand = PokerHand('4H 4D 4C 8S 8D').get_hand_name()
+    assert hand == 'Full House'
+
+def test_straight():
+    hand = PokerHand('2H 3C 4S 5H 6D').get_hand_name()
+    assert hand == 'Straight'
+
+def test_straight_4_cards():
+    hand = PokerHand('2H 3C 4S 5H 5C').get_hand_name()
+    assert hand == 'One Pair'
+
+'''
+Straight
+All five cards form a continuous sequence
+"2H 3C 4S 5H 6D", or "JD 8C 10S 9S 7D"
+An Ace can be low ("AS 2H 3C 4S 5D") or high ("10H JH QC KD AS")
+but not both at the same time.
+'''
