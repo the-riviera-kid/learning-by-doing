c5b1aab896578c866ca4ed17a79bfb3d99201997
Dan Jackson <dan.jackson@fox-it.com>
2023-08-14 11:05:15 +0200

    Football example and circle exercise.

---
+++ post-holiday-warmup/circles.py
@@ -0,0 +1,14 @@
+diameters = [10, 4, 83.2, 62, 33.3]
+
+def main():
+    '''
+        The 'diameters' list is a list of diameters of circles.
+        The diameter is in centimetres.
+        For each circle, print out the area of the circle.
+        The results should look like:
+            Circle with diameter 10 has an area of ???
+    '''
+    pass
+
+if __name__ == '__main__':
+    main()
