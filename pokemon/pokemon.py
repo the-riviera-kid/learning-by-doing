0d640834fb7c8e7f7a3696014fc7ecb084826dd0
Dan Jackson <dan.jackson@fox-it.com>
2023-08-21 11:50:05 +0200

    Taught everyone about opaque records, lookup tables, and Pokemon, which everybody loved very much.

---
+++ pokemon/pokemon.py
@@ -0,0 +1,68 @@
+def create(name, type, health):
+    return {
+        'name': name,
+        'type': type,
+        'health': health
+    }
+
+def describe(this_pokemon):
+    return (f"{this_pokemon['name']} is a "
+            f"{this_pokemon['type']} type pokemon, "
+            f"with {this_pokemon['health']} health")
+
+def battle(my_pokemon, their_pokemon):
+    # By default, a pokemon does 2 damage
+    # If you are strong against the other, damage == 4
+    # If you are weak against the other, damage == 1
+    # new_health = current_health - damage
+    # Pokemon with highest new health wins
+    # bulbasaur, grass , 5 vs squirtle, water, 5
+    # bulbasaur attacks squirtle
+    # bulbasaur (grass) is strong vs squirtle (water)
+    # so does 4 damage instead of 2
+    # squirtles new health = 5 - 4 (i.e. 1)
+    #
+    # squirtle attacks bulbasaur
+    # squirtle (water) is weak against bulbasaur (grass)
+    # so does 1 damage instead of 2
+    # bulbasaurs new health = 5 - 1 == 4
+    #
+    # bulbasaur (4 health) beats squirtle (1 health)
+
+    fire_table = {
+        'fire': 2,
+        'water': 1,
+        'grass': 4
+    }
+
+    water_table = {
+        'fire': 4,
+        'water': 2,
+        'grass': 1
+    }
+
+    grass_table = {
+        'fire': 1,
+        'water': 4,
+        'grass': 2
+    }
+
+    damage_table = {
+        'fire': fire_table,
+        'water': water_table,
+        'grass': grass_table
+    }
+
+    my_type = my_pokemon['type']
+    their_type = their_pokemon['type']
+    my_damage = damage_table[my_type][their_type]
+    their_damage = damage_table[their_type][my_type]
+
+    my_pokemon['health'] = my_pokemon['health'] - their_damage
+    their_pokemon['health'] = their_pokemon['health'] - my_damage
+
+    if my_pokemon['health'] > their_pokemon['health']:
+        return f"Yay! Your {my_pokemon['name']} beat their {their_pokemon['name']}!"
+    else:
+        return f"Oh no. Their {their_pokemon['name']} kicked {my_pokemon['name']}'s arse."
+
---
+++ pokemon/test_pokemon.py
@@ -0,0 +1,34 @@
+# grass is strong against water, weak against fire
+# water is strong against fire, weak against grass
+# fire is strong against grass, weak against water
+import pokemon
+
+def test_create_pokemon():
+    assert pokemon.create(
+        name='bulbasaur',
+        type='grass',
+        health=5)
+
+def test_what_is_in_a_pokemon():
+    squirtle = pokemon.create('squirtle', 'water', 5)
+    assert (pokemon.describe(squirtle) == 
+        'squirtle is a water type pokemon, with 5 health')
+
+def test_battle():
+    shaams_pokemon = pokemon.create('bulbasaur', 'grass', 5)
+    carlas_pokemon = pokemon.create('squirtle', 'water', 5)
+    result = pokemon.battle(
+        my_pokemon=shaams_pokemon,
+        their_pokemon=carlas_pokemon
+    )
+    assert result == 'Yay! Your bulbasaur beat their squirtle!'
+
+def test_battle_two():
+    shaams_pokemon = pokemon.create('bulbasaur', 'grass', 5)
+    carlas_pokemon = pokemon.create('charmander', 'fire', 5)
+    result = pokemon.battle(
+        my_pokemon=shaams_pokemon,
+        their_pokemon=carlas_pokemon
+    )
+    assert result == "Oh no. Their charmander kicked bulbasaur's arse."
+
